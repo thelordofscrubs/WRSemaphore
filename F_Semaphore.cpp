6e3a1f1c842a6cf8cd3b63c03ece404e90c3c04e
thelordofscrubs <ekaituri@gmail.com>
2023-09-01 19:25:52 -0500

    added non-busy thread sleeping, as well as inline comments

--- F_Semaphore.cpp
+++ F_Semaphore.cpp
@@ -1,33 +1,39 @@
 #include "WRSemaphore.h"
 
+// Constructor initializes the atomic variables counter and writer_waiting to 0.
 WRSemaphore::WRSemaphore() {
-    counter.store(0);
-    writer_waiting.store(0);
+    counter.store(0);          // Initialize reader counter to 0.
+    writer_waiting.store(0);   // Initialize writer waiting counter to 0.
 }
 
+// Function for a thread to attempt to read.
 void WRSemaphore::attempt_read() {
-    while(writer_waiting.load() > 0 || counter.load() < 0);
-
-    counter++;
-    return;
-
+    std::unique_lock<std::mutex> lk(cv_m);  // Acquire a unique lock on the mutex for the condition variable.
+    // Wait until no writer is waiting and no writer is in the critical section.
+    cv.wait(lk, [this] { return writer_waiting.load() == 0 && counter.load() >= 0; });
+    counter++;  // Increment the reader counter to indicate a new reader in the critical section.
 }
 
+// Function for a thread to indicate it has finished reading.
 void WRSemaphore::done_read(){
-    counter--;
+    counter--;  // Decrement the reader counter to indicate a reader has left the critical section.
+    cv.notify_all();  // Notify all waiting threads (likely writers) that they can attempt to acquire the lock.
 }
 
+// Function for a thread to attempt to write.
 void WRSemaphore::attempt_write() {
-    writer_waiting++;
-    while (counter > 0);
-    mtx.lock();
-    counter.store(-1);
-    writer_waiting--;
-    return;
+    std::unique_lock<std::mutex> lk(cv_m);  // Acquire a unique lock on the mutex for the condition variable.
+    writer_waiting++;  // Increment the writer waiting counter to give priority to writers.
+    // Wait until no readers or writers are in the critical section.
+    cv.wait(lk, [this] { return counter.load() <= 0; });
+    mtx.lock();  // Acquire the mutex lock for the critical section.
+    counter.store(-1);  // Set the counter to -1 to indicate a writer is in the critical section.
+    writer_waiting--;  // Decrement the writer waiting counter as the writer has acquired the lock.
 }
 
+// Function for a thread to indicate it has finished writing.
 void WRSemaphore::done_write() {
-    counter.store(0);
-    mtx.unlock();
-    return;
+    counter.store(0);  // Reset the counter to 0 to allow other threads to enter the critical section.
+    mtx.unlock();  // Release the mutex lock for the critical section.
+    cv.notify_all();  // Notify all waiting threads (readers and writers) that they can attempt to acquire the lock.
 }
--- WRSemaphore.h
+++ WRSemaphore.h
@@ -1,35 +1,46 @@
+// Preprocessor directive to prevent multiple inclusions of the header file.
 #ifndef MYCLASS_H
 #define MYCLASS_H
 
+// Macro to export or import classes and functions when building or using a DLL.
 #ifdef BUILD_DLL
-    #define DLL_EXPORT __declspec(dllexport)
+    #define DLL_EXPORT __declspec(dllexport)  // Exporting the class if building a DLL.
 #else
-    #define DLL_EXPORT __declspec(dllimport)
+    #define DLL_EXPORT __declspec(dllimport)  // Importing the class if using the DLL.
 #endif
 
+// Include necessary header files for multithreading and synchronization.
 #include <mutex>
 #include <atomic>
 #include <thread>
 #include <vector>
+#include <condition_variable>  // Include for condition variables.
 
-
+// Class definition for WRSemaphore.
 class DLL_EXPORT WRSemaphore {
 
     protected:
-    std::atomic<int> counter;
-    std::atomic<int> writer_waiting;
-    std::mutex mtx;
+    std::atomic<int> counter;         // Atomic counter for the number of readers.
+    std::atomic<int> writer_waiting;  // Atomic counter for the number of waiting writers.
+    std::mutex mtx;                   // Mutex for synchronizing write operations.
+    std::condition_variable cv;       // Condition variable for efficient thread waiting.
+    std::mutex cv_m;                  // Mutex for the condition variable.
+
+    // Placeholder functions for future use or subclassing.
     void start_read();
     void start_write();
 
     public:
+    // Constructor to initialize the WRSemaphore object.
     WRSemaphore();
 
-    void attempt_read();
-    void done_read();
+    // Public methods for read and write operations.
+    void attempt_read();  // Method for a thread to attempt to read.
+    void done_read();     // Method for a thread to indicate it has finished reading.
 
-    void attempt_write();
-    void done_write();
+    void attempt_write(); // Method for a thread to attempt to write.
+    void done_write();    // Method for a thread to indicate it has finished writing.
 };
 
+// End of preprocessor directive for multiple inclusions.
 #endif
